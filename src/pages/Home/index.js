26b972aa38dd8c856f16f44906d9367a7ad3bdf7
GMerencio <gmerencio.santos@gmail.com>
2021-11-27 04:41:39 -0300

    Ajustes no front-end

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
 import { Header } from '../Header';
+import { Horarios } from '../Horarios';
 
 export const Home = () => {
-  return <Header></Header>;
+  return (
+  	<div>
+  		<Header></Header>
+  		<Horarios></Horarios>
+  	</div>
+  );
 };
--- src/pages/Horarios/index.js
+++ src/pages/Horarios/index.js
@@ -1,3 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
 export const Horarios = () => {
-  return <h1>Horarios</h1>;
+
+	const [data, setData] = useState([]);
+	
+  const getAulas = async () => {
+    fetch("http://localhost/registreduca/api/controller/aulaController.php?findAll")
+      .then((response) => response.json())
+      .then((responseJson) => (
+        //console.log(responseJson),
+        getAulasDia(responseJson)
+      ));
+  }
+  
+  const getAulasDia = function(data) {
+  	let hoje = new Date();
+  	
+  	if(hoje.getDay() === 0)
+  		hoje.setDate(hoje.getDate() + 1);
+  		
+  	setData(data.filter(aula => new Date(aula.inicio).getDate() === hoje.getDate()));
+  }
+
+  useEffect(() => {
+    getAulas();
+  }, [])
+
+  return (
+  	<h1>Horários</h1>
+  );
 };
